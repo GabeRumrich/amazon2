7f2a13da62669644300ee5305faf730a897dc2d2
Gabe Rumrich <jgrumrich4@gmail.com>
2022-03-25 18:04:49 -0500

    Create-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Better than Amazon</a>
+      </header>
+      <main>Products</main>
+    </div>
+  );
+}
+
+export default App;

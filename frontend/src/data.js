6c26b2a48fb263e9394d2f6b50911c9669fce1cc
Gabe Rumrich <jgrumrich4@gmail.com>
2022-03-27 16:57:03 -0500

    pt 2

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'nike slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'shirts',
+      image: '/Images/nike shirt copy.jpg', //679 x 829
+      price: 25,
+      countInStock: 20,
+      brand: 'Nike',
+      rating: 4.9,
+      numReviews: 10,
+      description: 'High quality shirt',
+    },
+    {
+      name: 'Lululemon training shirt',
+      slug: 'Lululemon-training-shirt',
+      category: 'shirts',
+      image: '/Images/NEWlulu copy.jpg',
+      price: 69,
+      countInStock: 60,
+      brand: 'Lululemon',
+      rating: 5.0,
+      numReviews: 32,
+      description: 'Uber high quality shirt',
+    },
+    {
+      name: 'Lululemon dress pants',
+      slug: 'Lululemon-dress-pants',
+      category: 'Pants',
+      image: '/Images/LULUpants copy.jpg',
+      price: 125,
+      countInStock: 33,
+      brand: 'Lululemon',
+      rating: 5.0,
+      numReviews: 32,
+      description: 'Uber high quality Pants',
+    },
+    {
+      name: 'Birddogs training shorts',
+      slug: 'Birddogs-training-shorts',
+      category: 'shorts',
+      image: '/Images/amazon2BD copy 2.jpg',
+      price: 55,
+      countInStock: 2,
+      brand: 'Birddogs',
+      rating: 4.0,
+      numReviews: 32,
+      description: 'High quality shorts',
+    },
+  ],
+};
+
+export default data;
